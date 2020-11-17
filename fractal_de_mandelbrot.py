ddbe3a40bd7d971849a23292925701dd563c6b2b
Tubarão Cósmico <35200931+TubaraoCosmico@users.noreply.github.com>
2020-11-16 23:00:15 -0300

    Create fractal_de_mandelbrot.py

---
+++ fractal_de_mandelbrot.py
@@ -0,0 +1,90 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def definindo_cores(cor):
+    global imagem
+    # Color map 
+    # https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+    if cor == "hot":
+        plt.imshow(imagem,cmap='hot')
+    elif cor == "RdBu":
+        plt.imshow(imagem,cmap='RdBu')
+    elif cor == "xkcd":
+        plt.imshow(imagem)
+        plt.xkcd()
+    elif cor == "magma":
+        plt.imshow(imagem,cmap='magma')
+    elif cor == "plasma":
+        plt.imshow(imagem,cmap='plasma')
+    elif cor == "cividis":
+        plt.imshow(imagem,cmap='cividis')
+    elif cor == "inferno":
+        plt.imshow(imagem,cmap="inferno")
+    else:
+        plt.imshow(imagem)
+    
+    plt.colorbar()
+    plt.savefig(nomeArquivo + ".jpg")
+    plt.show()
+
+
+def atual_datahora(modo):
+    from datetime import datetime
+    resultado = datetime.today()
+    if modo == 1:
+        resultado = resultado.strftime('%d-%m-%Y_%H-%M-%S-%f')
+    else:
+        resultado = resultado.strftime('%d/%m/%Y %H:%M:%S:%f')
+    return resultado
+
+
+def iteracoes(c):
+    z = complex(0,0)  # valor inicial do z 
+    i = 0 
+    while( i < 127  and  abs(z*z) <= 2):
+        z = z*z + c
+        i = i + 1
+    return(i)
+
+
+def Imagem(rx, ry, ie, sd):
+    hx = (sd.real - ie.real)/rx
+    hy = (sd.imag - ie.imag )/ry 
+    matriz = []
+    for i in range(rx):
+        linha = []
+        y = ie.imag + i*hy
+        for j in range(ry):
+            x = ie.real + j*hx
+            cc = complex(x,y)
+            valor = iteracoes(cc)
+            linha.append(valor)
+        matriz.append(linha)
+    # print( np.matrix(matriz))
+    np.savetxt(nomeArquivo, np.matrix(matriz), fmt='%.0f')
+
+
+# Verificando se todos os argumentos  foram passados 
+if len(sys.argv) >= 7:
+    argumentos = sys.argv
+    # argumentos[0] = nome do arquivo python
+    # argumentos[1] = ax (parte real do ponto inferior esquerdo)
+    # argumentos[2] = ay (parte imaginária do ponto inferior esquerdo)
+    # argumentos[3] = bx (parte real do ponto superior direito)
+    # argumentos[4] = by (parte imaginária do ponto superior direito)
+    # argumentos[5] = resolução em x 
+    # argumentos[6] = resolução em y
+    nomeArquivo = "ax=" + argumentos[1] + "__ay=" + argumentos[2] + "__bx=" + argumentos[3] + "__by=" + argumentos[4] + "__" + atual_datahora(1) + ".txt"
+    a = complex(float(argumentos[1]), float(argumentos[2]))
+    b = complex(float(argumentos[3]), float(argumentos[4]))
+    r = [int(argumentos[5]), int(argumentos[6])]
+    Imagem(r[0], r[1], a,b)
+    print("Gerado arquivo de saida: " + nomeArquivo)
+    imagem = np.loadtxt(nomeArquivo)
+    if len(sys.argv) == 8:
+        definindo_cores(argumentos[7])
+    else:
+        definindo_cores("padrao")
+else:
+    print("Você não informou todos os argumentos")
